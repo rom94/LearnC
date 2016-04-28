a7587a11d3fa399c31512410c659bf361c2fc768
Romain De Page <rpage@levallois.eudoweb.com>
2016-04-28 17:32:06 +0200

    Projet TPWeekend terminé

---
+++ MaPremiereAppli/TPWeekEnd/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWeekEnd
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                AfficherBonWeekend();
+            }
+            else
+            {
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Monday && DateTime.Now.Hour < 9)
+                {
+                    AfficherBonWeekend();
+                }
+                else
+                {
+                    if (DateTime.Now.Hour >= 9 && DateTime.Now.Hour < 18)
+                    {
+                        AfficherBonneJournee();
+                    }
+                    else
+                    {
+                        if (DateTime.Now.Hour >= 18 && DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+                        {
+                            AfficherBonWeekend();
+                        }
+                        else
+                        {
+                            AfficherBonneSoiree();
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void AfficherBonWeekend ()
+        {
+            Console.WriteLine("Bon week-end " + Environment.UserName);
+        }
+
+        private static void AfficherBonneJournee ()
+        {
+            Console.WriteLine("Bonjour " + Environment.UserName);
+        }
+
+        private static void AfficherBonneSoiree ()
+        {
+            Console.WriteLine("Bonsoir " + Environment.UserName);
+        }
+    }
+}
