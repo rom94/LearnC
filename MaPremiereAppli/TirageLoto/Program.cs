35e8376e1ee2878f496b04f271ac995d2e384d0d
Romain De Page <romaindepage@gmail.com>
2016-05-16 11:23:09 +0200

    Ajout du code du TP loto

--- MaPremiereAppli/TirageLoto/Program.cs
+++ MaPremiereAppli/TirageLoto/Program.cs
@@ -10,6 +10,29 @@ namespace TirageLoto
     {
         static void Main (string[] args)
         {
+            TirageLoto();
+        }
+
+        private static void TirageLoto ()
+        {
+            int[] tirage = new int[7]; //Initialisation de mon tableau
+
+            Random random = new Random();
+
+            for (int i = 0; i < tirage.Length; i++)
+            {
+                int numero;
+
+                do
+                {
+                    numero = random.Next(1, 49);
+
+                } while (tirage.Contains(numero));
+
+                tirage[i] = numero;
+
+                Console.WriteLine(tirage[i]);
+            }
         }
     }
 }
