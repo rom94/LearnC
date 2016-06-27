2796210f3a1f6e7f02aefb222d49adcfe16c5c43
Romain De Page <romaindepage@gmail.com>
2016-06-27 16:20:31 +0200

    Fin du TP d'apprentissage C#

--- MaPremiereAppli/MaPremiereAppli/Program.cs
+++ MaPremiereAppli/MaPremiereAppli/Program.cs
@@ -168,7 +168,7 @@ namespace MaPremiereAppli
             }
             Console.WriteLine(somme);*/
 
-            Console.WriteLine("Voulez-vous continuer ? (0/N)");
+            /*Console.WriteLine("Voulez-vous continuer ? (0/N)");
             ConsoleKeyInfo saisie = Console.ReadKey(true);
             if (saisie.Key == ConsoleKey.O)
             {
@@ -177,9 +177,55 @@ namespace MaPremiereAppli
             else
             {
                 Console.WriteLine("On s'arrête");
-            }
+            }*/
 
+            /*foreach (string parametre in args)
+            {
+                Console.WriteLine(parametre);
+            }*/
 
+            /*int i = 0;
+            int j = 0;
+            int largeur = 21;
+            int hauteur = 4;
+            Console.WriteLine(@"      .--------.");
+            Console.WriteLine(@" ____/_____|___ \___");
+            Console.WriteLine(@"O    _   - |   _   ,*");
+            Console.WriteLine(@" '--(_)-------(_)--'");
+            while (true)
+            {
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                switch (info.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (i > 0)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i - 1, j);
+                            i--;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (i < Console.WindowWidth - largeur)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i + 1, j);
+                            i++;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (j > 0)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i, j - 1);
+                            j--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.MoveBufferArea(i, j, largeur, hauteur, i, j + 1);
+                        j++;
+                        break;
+                }
+                if (info.Key == ConsoleKey.Q)
+                    break;
+            }*/
 
         }
 
