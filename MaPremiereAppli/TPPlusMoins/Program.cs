3865499ec434db6b542e27f53d8ce716bc461b41
Romain De Page <romaindepage@gmail.com>
2016-06-27 15:37:52 +0200

    Ajout du programme du TP

--- MaPremiereAppli/TPPlusMoins/Program.cs
+++ MaPremiereAppli/TPPlusMoins/Program.cs
@@ -10,6 +10,42 @@ namespace TPPlusMoins
     {
         static void Main (string[] args)
         {
+            int valeurATrouver = new Random().Next(0, 101);
+            int compteur = 0;
+            bool trouve = false;
+
+            Console.WriteLine("Veuillez saisir un nombre entre 0 et 100 : ");
+
+            while (!trouve)
+            {
+                string saisie = Console.ReadLine();
+                int valeurSaisie;
+
+                if (int.TryParse(saisie, out valeurSaisie))
+                {
+                    if (valeurSaisie == valeurATrouver)
+                    {
+                        trouve = true;
+                    }
+                    else
+                    {
+                        if (valeurSaisie < valeurATrouver)
+                        {
+                            Console.WriteLine("Trop petit");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Trop grand");
+                        }
+                    }
+                    compteur++;
+                }
+                else
+                {
+                    Console.WriteLine("La valeur saisie est incorrecte, veuillez réessayer");
+                }
+            }
+            Console.WriteLine("Vous avez trouvé en " + compteur + " coups !");
         }
     }
 }
